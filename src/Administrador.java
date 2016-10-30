8cd83114eb164129bd4b7f9f0e598e412b5b4ccc
Heredia <Heredia@10.0.2.15>
2016-10-30 12:08:52 +0100

    administraador

---
+++ src/Administrador.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author edilberto
+ */
+import BaseDatos.ConexionMySQL;
+import java.sql.*;
+
+public class Administrador {
+    private static Administrador admin;
+    private final String nombre;
+    public static Administrador crearAdministrador(String nombre) {
+        if (admin==null) {
+            admin=new Administrador(nombre);
+        }
+        return admin;
+    }
+
+    private Administrador(String nombre){
+        //base.cargar("baseVacia.xml");
+        this.nombre=nombre;
+        //init();
+    }
+    public void ingresarEmpleado(String nombre, String apellidos, float sueldo, int ci, String correo, String tipo){
+        //despues verificar si crear una tabla de sueldos de acuerdo al tipo 
+        ConexionMySQL base = new ConexionMySQL();
+        Connection cn = base.Conectar();
+        String ingreso1 = "INSERT INTO empleado"+"(Nombre, Apellidos, sueldo, ci, correo, tipo)"+
+                "VALUES(?,?,?,?,?,?)";
+        try{
+            int n;
+            PreparedStatement ps=cn.prepareStatement(ingreso1);
+            ps.setString(1, nombre);
+            ps.setString(2, apellidos);
+            ps.setFloat(3, sueldo);
+            ps.setInt(4, ci);
+            ps.setString(5, correo);
+            ps.setString(6, tipo);
+            n=ps.executeUpdate();
+            if(n>0) System.out.println("Funciono");
+            else System.out.println("NOOOOO");
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+        }
+        
+    }
+}
---
+++ src/Prueba.java
@@ -0,0 +1,11 @@
+
+/**
+ *
+ * @author Heredia
+ */
+public class Prueba {
+    public static void main(String[] args){
+        Administrador admin=Administrador.crearAdministrador("");
+        admin.ingresarEmpleado("Chino", "POma", 15, 200, "asdf", "Venta");
+    }
+}
